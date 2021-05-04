029a0cb055e0fa7a477a83caf49e5ecb3ed88b43
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-05-04 09:36:19 -0400

    add NominationContext to Banner

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,20 @@ import MovieList from './components/MovieList'
 import Banner from './components/Banner'
 import InfoButton from './components/InfoButton'
 import Media, { useMedia } from 'react-media';
+import NominationContextProvider from './context/NominationContext';
 
 function App() {
-
   const isSmallScreen = useMedia({ query: "(max-width: 667px)" });
-
-  const [nominations, setNominations] = useState([])
 
 
   return (
-    
-    <div className="container">
-    <Banner nominations={nominations} />
-    <MovieList />
-    {isSmallScreen && <InfoButton />}
-    </div>
+    <NominationContextProvider>
+      <div className="container">
+      <Banner />
+      <MovieList />
+      {isSmallScreen && <InfoButton />}
+      </div>
+    </NominationContextProvider>
   );
 }
 export default App;
