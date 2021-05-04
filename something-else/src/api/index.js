fc6ada8e36f31263c3325a6241aaa31e2e70d1aa
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-05-03 22:14:38 -0400

    commit on something-else

---
+++ something-else/src/api/index.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+const url = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API_KEY}`
+
+const defaultQuery = {
+  title: 'Star Wars',
+  year: null,
+  plotLength: null
+}
+
+export const parseQuery = (query) => {
+  const queryTitle = '&t=' + query.title.split(' ').join('+')
+  const queryYear = query.year ? `&y=${query.year}` : ''
+
+  return url + queryTitle + queryYear
+}
+
+export const fetchMovies = async (query=defaultQuery) => {
+  const parsedQuery = parseQuery(query)
+
+  try {
+    const { data } = await axios.get(parsedQuery)
+    return data
+
+  } catch(error) {
+    console.log(error)
+  }
+} 
