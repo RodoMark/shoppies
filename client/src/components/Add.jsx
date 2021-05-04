66e4090737392300c31188a256b60a39fdd7db84
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-05-04 15:46:50 -0400

    map results in Add component

--- client/src/components/Add.jsx
+++ client/src/components/Add.jsx
@@ -1,10 +1,60 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { fetchMovies, parseQuery } from '../api'
+
+import { TextField } from '@material-ui/core'
 
 const Add = () => {
+  const [title, setTitle] = useState('')
+  const [year, setYear] = useState('')
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const callAPI = async () => {
+      try {
+        const result = await fetchMovies({title, year})
+      setResults([result])
+      } catch(error) {
+        setResults([])
+      }
+      
+    }
+
+    callAPI()
+  }, [title, year])
+
   return (
-    <div>
+    <section className="add-page">
+      <div className="container">
+        <article className="add-content">
+          <div className="input-wrapper">
+            <TextField 
+              onChange={(event) => setTitle(event.target.value)}
+              id="standard-search" 
+              placeholder="Movie Title"
+              label="Movie Title" 
+              type="search" 
+            />
+            <TextField 
+              onChange={(event) => setYear(event.target.value)}
+              id="standard-search"
+              placeholder="Year"
+              label="Year" 
+              type="search" 
+            />
+          </div>
+
+          {results.length > 0 && (
+            <ul className="results">
+              {results.map(movie => (
+                <li>{movie.Title}</li>
+              ))}
+            </ul>
+          ) }
+        </article>
+      </div>
       <h1>Add</h1>
-    </div>
+    </section>
   )
 }
 
