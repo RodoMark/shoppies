55caa90793e0b87c9ff2cecdbf059bb226c0e8a9
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-05-04 23:05:16 -0400

    add film search test

--- client/cypress/integration/search.spec.js
+++ client/cypress/integration/search.spec.js
@@ -2,6 +2,21 @@ describe("renders the search page", () => {
   it("renders correctly", () => {
     cy.visit('/search')
     cy.get('#film-search-label').should('exist')
-    cy.get('#year-search-label').should('exist')
+  })
+
+  it("begins to search when you type", async () => {
+    cy.visit('/search')
+      .get('#film-search-label')
+      .should('exist')
+    
+      .get('#film-search')
+      .type('Star')
+
+      const search = await cy
+        .get('div.container > ul > li:nth-child(1)')
+        .contains('Episode IV - A New Hope')
+        .get('.btn')
+        .contains('Add To Nominations')
+    
   })
 })
