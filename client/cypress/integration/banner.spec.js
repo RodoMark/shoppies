c79ac8d3fbed7d1a4dd65ee203f0f8250f086ced
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-05-04 23:44:15 -0400

    add banner complete test

--- client/cypress/integration/banner.spec.js
+++ client/cypress/integration/banner.spec.js
@@ -19,20 +19,31 @@ describe("the banner", () => {
     cy.get('.banner')
       .contains('5')
     
-    .get('#film-search')
-    .type('Star')
-
-    await cy
-      .get('div.container > ul > li:nth-child(1)')
-      
-    cy.contains('Episode IV - A New Hope')
-      .get('.btn')
-      .contains('Add To Nominations')
-      .click()
-    
-
-
-
-    
+      .get('#film-search')
+      .type('Star')
+      .wait(1200)
   })
+
+  it("displays a confirmation message once you choose 5 nominations", async () => {
+    cy.visit('/search')
+
+    cy.get('.banner')
+      .contains('5')
+    
+      .get('#film-search')
+      .type('Star')
+      .wait(1200)
+
+    cy.get('div.container > ul > li')
+    .each(() => {
+      cy.get('.btn')
+      .contains('Add To Nominations')
+      .click()        
+    }, 4)
+
+    cy.get('.banner')
+      .contains('You have nominated 5 movies.')
+  })
+    
+
 })
