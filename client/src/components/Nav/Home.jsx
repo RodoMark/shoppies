297f0a28d0cd612a8a850f2e6fe0f78865a69f8c
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-05-06 22:24:46 -0400

    add 'start here' button to Home

--- client/src/components/Nav/Home.jsx
+++ client/src/components/Nav/Home.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 const Home = () => {
+  const history = useHistory()
+
+  const handleClick = () => {
+    history.push('/search')
+  }
+
   return (
       <div className="container">
         <section className="stage splash">
         <h1>WELCOME TO SHOPPIES</h1>
-        <h2>You can search for your favourite films and nominate them for the upcoming Shoppies Awards</h2>
+        <h2>You can search for your favourite films and nominate them for the upcoming Shoppies Awards. <br /> Start by <button className="btn btn--confirm" onClick={() => handleClick()}>clicking here</button></h2>
         <h2>Tech Stack:</h2>
         <ul className="tech-stack">
           <li>Front-end: React, react-router, Reducer, Axios, Material-UI, SASS</li>
